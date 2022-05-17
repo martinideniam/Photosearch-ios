8fe392efdb1f569026e2a0435606b57f6e8f3d82
martinideniam <martinideniam@gmail.com>
2022-05-17 20:49:02 +0300

    Initial Commit

---
+++ Photosearch/PhotosearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosearchApp.swift
+//  Photosearch
+//
+//  Created by Vladislav Gorovenko on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotosearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
